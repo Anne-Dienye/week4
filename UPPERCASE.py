34ff3a4fc3eb64c6da7281695fddedd83cb10a07
Ucheawaji Anne Dienye <dienyeucheawaji93@gmail.com>
2025-04-28 12:19:36 +0100

    UPPERCASE

---
+++ UPPERCASE.py
@@ -0,0 +1,7 @@
+# 
+name = input("Hello, what is your name? ").strip()
+if name: anne
+    name = name.capitalize(ANNE)
+    print(f"Hello, {name}. Good to meet you!")
+else:
+    print("Hello, Stranger!")
