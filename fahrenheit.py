44eb409b48d9c4a25b9aabaf4a4eb711cbc409f2
Anne-Dienye <dienyeucheawaji93@gmail.com>
2025-05-09 00:21:08 +0100

    Delete fahrenheit.py

--- fahrenheit.py
+++
@@ -1,8 +0,0 @@
-#coding
-temp_input = input("Enter temperature in centigrade (e.g., 32C): ").strip()
-if temp_input[-1] == 'C':
-    temp_c = float(temp_input[:-1])
-    temp_f = celsius_to_fahrenheit(temp_c)
-    print(f"{temp_input} is equivalent to {temp_f}F.")
-else:
-    print("Invalid input format. Please use 'numberC'.")
